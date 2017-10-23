3ef2215456d96325249c63faaa37baf2436b4dcd
Sven Tägtmeier <s.taegtmeier@fielmann.com>
2017-10-23 07:35:06 +0200

    change docker

--- docker.sh
+++ docker.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
 
+docker run -d -v /data/mysql:/var/lib/mysql -v /data/log:/var/log/mysql -p 3306:3306 --name biz-balance_mysql  biz-balance/mysql
 
-docker run -ti -d -p 2253:80 -v /var/www/html:/var/www/biz-balance --name biz-balance apache22php53 /bin/bash
+docker run -ti -d -p 2253:80 -v /var/www/html:/var/www/biz-balance --name biz-balance_apache biz-balance/apache /bin/bash
