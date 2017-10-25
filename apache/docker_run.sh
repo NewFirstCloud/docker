5b96463e5e6e2fca46123307877d51773a75da59
Sven Taegtmeier <sven@taegtmeier.net>
2017-10-25 20:50:08 +0200

    change apache - docker_run

--- apache/docker_run.sh
+++ apache/docker_run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker build -t biz_balance_apache .
+docker build -t biz_balance-apache .
 
-docker run -ti -d -p 80:80 -p 443:443 -v /srv/www:/var/www/biz-balance --name biz_balance-apache biz-balance/apache /bin/bash
+docker run -ti -d -p 80:80 -p 443:443 -v /srv/www:/var/www/biz-balance --name biz_balance-apache biz_balance-apache /bin/bash
