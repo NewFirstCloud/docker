20ec33da908b6e382b6bda9d19dbc8050074f969
Sven Taegtmeier <sven@taegtmeier.net>
2017-11-12 02:04:59 +0100

    finally it works!

--- mysql/docker_run.sh
+++ mysql/docker_run.sh
@@ -2,4 +2,4 @@
 
 docker build -t biz_balance-mysql .
 
-docker run -ti -d -p 3306:3306 -v /srv/mysql:/var/lib/mysql --name biz_balance-mysql biz_balance-mysql /bin/bash
+docker run -ti -d -p 3306:3306 -p 443:443 -p 80:80 -v /srv/mysql:/var/lib/mysql -v /srv/www:/var/www --name biz_balance-mysql biz_balance-mysql /bin/bash
